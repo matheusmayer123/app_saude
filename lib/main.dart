2854eedd89d4e3c81a09d146b87eac376010c2c9
Matheus Mayer <mafrezza17@gmail.com>
2024-04-19 14:59:51 -0300

    Alteração da Arquitetura

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,7 @@
-import 'package:app_saude/user_form.dart';
+import 'package:app_saude/myapp.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'App Saúde',
-      theme: ThemeData(),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(top: 20),
-              child: const Text(
-                'Bem-vindo ao App Saúde',
-                style: TextStyle(
-                    color: Color.fromRGBO(67, 136, 131, 1.0),
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-          ],
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.only(top: 20),
-              child: const CircleAvatar(
-                radius: 100,
-                backgroundColor: Colors.greenAccent,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const UserForm(),
-          ],
-        ),
-      ),
-    );
-  }
 }
---
+++ lib/myapp.dart
@@ -0,0 +1,15 @@
+import 'package:app_saude/homepage.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App Saúde',
+      theme: ThemeData(),
+      home: const MyHomePage(),
+    );
+  }
+}
