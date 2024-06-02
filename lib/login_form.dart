a57a7412edbccf776d74873a5e9a3cad8f0e8296
RuBeNsMoTa13 <111132440+RuBeNsMoTa13@users.noreply.github.com>
2024-06-02 05:05:58 -0300

    funcinou login do usuario

--- lib/login_form.dart
+++ lib/login_form.dart
@@ -1,15 +1,12 @@
-// ignore_for_file: prefer_const_constructors
-
-import 'package:app_saude/pages/contact_list_page.dart';
-import 'package:app_saude/field_form.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:app_saude/providers/user_provider.dart';
+import 'package:app_saude/pages/home_page.dart';
 import 'package:app_saude/pages/registerpage.dart';
 import 'package:app_saude/pages/registerpage_medico.dart';
-import 'package:flutter/material.dart';
-
-import 'pages/forgot_password.dart';
 
 class LoginForm extends StatefulWidget {
-  const LoginForm({super.key});
+  const LoginForm({Key? key}) : super(key: key);
 
   @override
   State<LoginForm> createState() => _LoginFormState();
@@ -18,43 +15,57 @@ class LoginForm extends StatefulWidget {
 class _LoginFormState extends State<LoginForm> {
   TextEditingController controllerCPF = TextEditingController();
   TextEditingController controllerPassword = TextEditingController();
-  String btnPassword = '';
+
+  void _login(UserProvider userProvider) async {
+    String cpf = controllerCPF.text;
+    String password = controllerPassword.text;
+
+    // Autenticação
+    String result = await userProvider.authenticateUser(cpf, password);
+
+    if (result == 'Autenticação bem-sucedida') {
+      // Navegue para a página inicial após o login
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(result),
+          duration: Duration(seconds: 3),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    final userProvider = Provider.of<UserProvider>(context);
+
     return Center(
       child: Padding(
         padding: const EdgeInsets.all(25),
         child: Column(
           children: [
-            SizedBox(
-              height: 10,
-            ),
-            FieldForm(
-              label: 'CPF',
-              isPassword: false,
+            SizedBox(height: 10),
+            TextField(
               controller: controllerCPF,
+              decoration: InputDecoration(labelText: 'CPF'),
             ),
-            SizedBox(
-              height: 10,
-            ),
-            FieldForm(
-              label: 'Senha',
-              isPassword: true,
+            SizedBox(height: 10),
+            TextField(
               controller: controllerPassword,
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Senha'),
             ),
-            SizedBox(
-              height: 15,
-            ),
+            SizedBox(height: 15),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 TextButton(
                   onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => EsqueceuSenha()));
+                    // Implemente a recuperação de senha se necessário
                   },
                   style: TextButton.styleFrom(
                     minimumSize: Size.zero,
@@ -77,12 +88,7 @@ class _LoginFormState extends State<LoginForm> {
                     height: 80,
                     width: 350,
                     child: TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => ViewUserPage()));
-                      },
+                      onPressed: () => _login(userProvider),
                       style: ButtonStyle(
                         backgroundColor: MaterialStateProperty.all(
                           Color.fromRGBO(62, 124, 120, 1.0),
@@ -97,20 +103,14 @@ class _LoginFormState extends State<LoginForm> {
                       ),
                     ),
                   ),
-                  const SizedBox(
-                    height: 30,
-                  ),
+                  const SizedBox(height: 30),
                   TextButton(
-                    style: TextButton.styleFrom(
-                      minimumSize: Size.zero,
-                      padding: EdgeInsets.zero,
-                      tapTargetSize: MaterialTapTargetSize.padded,
-                    ),
                     onPressed: () {
+                      // Navegue para a página de registro de usuário
                       Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => RegisterPage()));
+                        context,
+                        MaterialPageRoute(builder: (context) => RegisterPage()),
+                      );
                     },
                     child: const Text(
                       'Cadastre-se aqui!',
@@ -118,19 +118,16 @@ class _LoginFormState extends State<LoginForm> {
                     ),
                   ),
                   TextButton(
-                    style: TextButton.styleFrom(
-                      minimumSize: Size.zero,
-                      padding: EdgeInsets.zero,
-                      tapTargetSize: MaterialTapTargetSize.padded,
-                    ),
                     onPressed: () {
+                      // Navegue para a página de registro de médico
                       Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => RegisterPageMedico()));
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => RegisterPageMedico()),
+                      );
                     },
                     child: const Text(
-                      'Cadastre-se aqui Medico',
+                      'Cadastre-se aqui Médico',
                       style: TextStyle(color: Colors.blueGrey),
                     ),
                   ),
