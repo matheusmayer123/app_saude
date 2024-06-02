3116fead9eba15f3ec9906dfe511c7316963aab9
RuBeNsMoTa13 <111132440+RuBeNsMoTa13@users.noreply.github.com>
2024-06-02 02:58:26 -0300

    Update registerpage_medico.dart

--- lib/pages/registerpage_medico.dart
+++ lib/pages/registerpage_medico.dart
@@ -1,9 +1,10 @@
-import 'package:app_saude/dbconnection/MongoDbModel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:mongo_dart/mongo_dart.dart' as M;
+import 'package:app_saude/dbconnection/MongoDbModel.dart';
 import 'package:app_saude/pages/created_account.dart';
 import 'package:app_saude/providers/medico_provider.dart';
+import 'package:flutter_masked_text2/flutter_masked_text2.dart';
 
 import '../field_form.dart';
 
@@ -15,15 +16,29 @@ class RegisterPageMedico extends StatefulWidget {
 }
 
 class _RegisterPageMedicoState extends State<RegisterPageMedico> {
+  final _formKey = GlobalKey<FormState>();
+
   TextEditingController controllerNome = TextEditingController();
   TextEditingController controllerSobrenome = TextEditingController();
   TextEditingController controllerEmail = TextEditingController();
-  TextEditingController controllerCPF = TextEditingController();
+  var controllerCPF = MaskedTextController(mask: '000.000.000-00');
   TextEditingController controllerRua = TextEditingController();
   TextEditingController controllerNumeroCasa = TextEditingController();
   TextEditingController controllerBairro = TextEditingController();
   TextEditingController controllerSenha = TextEditingController();
   TextEditingController controllerCRM = TextEditingController();
+
+  String? _validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Email é obrigatório';
+    }
+    // Regex para validar o formato do email
+    final regex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
+    if (!regex.hasMatch(value)) {
+      return 'Formato de email inválido';
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,104 +56,161 @@ class _RegisterPageMedicoState extends State<RegisterPageMedico> {
         child: Padding(
           padding: const EdgeInsets.all(25),
           child: SingleChildScrollView(
-            child: Column(
-              children: [
-                FieldForm(
-                  label: 'CRM',
-                  isPassword: false,
-                  controller: controllerCRM,
-                ),
-                const SizedBox(height: 10),
-                FieldForm(
-                  label: 'Nome',
-                  isPassword: false,
-                  controller: controllerNome,
-                ),
-                const SizedBox(height: 10),
-                FieldForm(
-                  label: 'Sobrenome',
-                  isPassword: false,
-                  controller: controllerSobrenome,
-                ),
-                const SizedBox(height: 10),
-                FieldForm(
-                  label: 'Email',
-                  isPassword: false,
-                  controller: controllerEmail,
-                ),
-                const SizedBox(height: 10),
-                FieldForm(
-                  label: 'CPF',
-                  isPassword: false,
-                  controller: controllerCPF,
-                ),
-                const SizedBox(height: 10),
-                FieldForm(
-                  label: 'Rua',
-                  isPassword: false,
-                  controller: controllerRua,
-                ),
-                const SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: 150,
-                      child: FieldForm(
-                        label: 'Número',
-                        isPassword: false,
-                        controller: controllerNumeroCasa,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 150,
-                      child: FieldForm(
-                        label: 'Bairro',
-                        isPassword: false,
-                        controller: controllerBairro,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                FieldForm(
-                  label: 'Senha',
-                  isPassword: true,
-                  controller: controllerSenha,
-                ),
-                const SizedBox(height: 10),
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: SizedBox(
-                    height: 80,
-                    width: 200,
-                    child: Container(
-                      margin: const EdgeInsets.only(top: 15),
-                      child: TextButton(
-                        onPressed: () async {
-                          await _insertData(context);
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  const ConfirmarEmailContaNova(),
-                            ),
-                          );
-                        },
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(
-                            const Color.fromRGBO(62, 124, 120, 1.0),
-                          ),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  FieldForm(
+                    label: 'CRM',
+                    isPassword: false,
+                    controller: controllerCRM,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'CRM é obrigatório';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  FieldForm(
+                    label: 'Nome',
+                    isPassword: false,
+                    controller: controllerNome,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Nome é obrigatório';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  FieldForm(
+                    label: 'Sobrenome',
+                    isPassword: false,
+                    controller: controllerSobrenome,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Sobrenome é obrigatório';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  FieldForm(
+                    label: 'Email',
+                    isPassword: false,
+                    controller: controllerEmail,
+                    validator: _validateEmail,
+                  ),
+                  const SizedBox(height: 10),
+                  FieldForm(
+                    label: 'CPF',
+                    isPassword: false,
+                    controller: controllerCPF,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'CPF é obrigatório';
+                      }
+                      if (value.length != 14) {
+                        return 'CPF deve ter 11 dígitos';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  FieldForm(
+                    label: 'Rua',
+                    isPassword: false,
+                    controller: controllerRua,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Rua é obrigatório';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: 150,
+                        child: FieldForm(
+                          label: 'Número',
+                          isPassword: false,
+                          controller: controllerNumeroCasa,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Número é obrigatório';
+                            }
+                            return null;
+                          },
                         ),
-                        child: const Text(
-                          'Salvar',
-                          style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      SizedBox(
+                        width: 150,
+                        child: FieldForm(
+                          label: 'Bairro',
+                          isPassword: false,
+                          controller: controllerBairro,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Bairro é obrigatório';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  FieldForm(
+                    label: 'Senha',
+                    isPassword: true,
+                    controller: controllerSenha,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Senha é obrigatória';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: SizedBox(
+                      height: 80,
+                      width: 200,
+                      child: Container(
+                        margin: const EdgeInsets.only(top: 15),
+                        child: TextButton(
+                          onPressed: () async {
+                            if (_formKey.currentState!.validate()) {
+                              await _insertData(context);
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      const ConfirmarEmailContaNova(),
+                                ),
+                              );
+                            }
+                          },
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(
+                              const Color.fromRGBO(62, 124, 120, 1.0),
+                            ),
+                          ),
+                          child: const Text(
+                            'Salvar',
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
